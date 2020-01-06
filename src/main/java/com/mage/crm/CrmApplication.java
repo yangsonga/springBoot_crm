ff920d510c49004f62a7ddea8571355e25c1cd15
Tonygogo <kl2422@163.com>
2020-01-06 16:51:42 +0800

    添加哈哈内容

--- src/main/java/com/mage/crm/CrmApplication.java
+++ src/main/java/com/mage/crm/CrmApplication.java
@@ -1,14 +1,17 @@
 package com.mage.crm;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.mage.crm.mapper") // 扫描MyBatis的mapper接口
+@Slf4j
 public class CrmApplication {
 
     public static void main(String[] args) {
+        log.info("hahahaha...");
         SpringApplication.run(CrmApplication.class, args);
 
     }
